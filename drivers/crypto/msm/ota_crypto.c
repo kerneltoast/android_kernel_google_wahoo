3b51b19ee0cc9b2cae6dcf645410f288e8352966
Sultan Alsawaf <sultanxda@gmail.com>
2016-08-19 23:22:10 -0700

    crypto: msm: Don't bail out when debugfs creation fails
    Debugfs creation failure is a non-fatal error. Don't kill crypto when
    this happens.
    
    Skip debugfs creation entirely when CONFIG_DEBUG_FS is disabled.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/crypto/msm/ota_crypto.c
+++ drivers/crypto/msm/ota_crypto.c
@@ -139,9 +139,11 @@ struct qcota_stat {
 	u64 f9_op_fail;
 };
 static struct qcota_stat _qcota_stat;
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *_debug_dent;
 static char _debug_read_buf[DEBUG_MAX_RW_BUF];
 static int _debug_qcota;
+#endif
 
 static struct ota_dev_control *qcota_control(void)
 {
@@ -791,6 +793,7 @@ static struct platform_driver qcota_plat_driver = {
 	},
 };
 
+#ifdef CONFIG_DEBUG_FS
 static int _disp_stats(void)
 {
 	struct qcota_stat *pstat;
@@ -942,15 +945,15 @@ err:
 	debugfs_remove_recursive(_debug_dent);
 	return rc;
 }
+#endif
 
 static int __init qcota_init(void)
 {
-	int rc;
 	struct ota_dev_control *podev;
 
-	rc = _qcota_debug_init();
-	if (rc)
-		return rc;
+#ifdef CONFIG_DEBUG_FS
+	_qcota_debug_init();
+#endif
 
 	podev = &qcota_dev;
 	INIT_LIST_HEAD(&podev->ready_commands);
@@ -964,7 +967,9 @@ static int __init qcota_init(void)
 }
 static void __exit qcota_exit(void)
 {
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(_debug_dent);
+#endif
 	platform_driver_unregister(&qcota_plat_driver);
 }
 
--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -152,9 +152,11 @@ struct qcedev_stat {
 };
 
 static struct qcedev_stat _qcedev_stat;
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *_debug_dent;
 static char _debug_read_buf[DEBUG_MAX_RW_BUF];
 static int _debug_qcedev;
+#endif
 
 static struct qcedev_control *qcedev_minor_to_control(unsigned n)
 {
@@ -2000,6 +2002,7 @@ static struct platform_driver qcedev_plat_driver = {
 	},
 };
 
+#ifdef CONFIG_DEBUG_FS
 static int _disp_stats(int id)
 {
 	struct qcedev_stat *pstat;
@@ -2089,20 +2092,22 @@ err:
 	debugfs_remove_recursive(_debug_dent);
 	return rc;
 }
+#endif
 
 static int qcedev_init(void)
 {
-	int rc;
+#ifdef CONFIG_DEBUG_FS
+	_qcedev_debug_init();
+#endif
 
-	rc = _qcedev_debug_init();
-	if (rc)
-		return rc;
 	return platform_driver_register(&qcedev_plat_driver);
 }
 
 static void qcedev_exit(void)
 {
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(_debug_dent);
+#endif
 	platform_driver_unregister(&qcedev_plat_driver);
 }
 
