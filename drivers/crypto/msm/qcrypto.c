c907e89225308dd3b837e980bf03b972f7a6a5b4
Sultan Alsawaf <sultan@kerneltoast.com>
2016-08-19 23:22:10 -0700

    crypto: msm: Don't bail out when debugfs creation fails
    Debugfs creation failure is a non-fatal error. Don't kill crypto when
    this happens.
    
    Skip debugfs creation entirely when CONFIG_DEBUG_FS is disabled.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -122,8 +122,10 @@ struct crypto_stat {
 	u64 ahash_op_fail;
 };
 static struct crypto_stat _qcrypto_stat;
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *_debug_dent;
 static char _debug_read_buf[DEBUG_MAX_RW_BUF];
+#endif
 static bool _qcrypto_init_assign;
 struct crypto_priv;
 struct qcrypto_req_control {
@@ -1103,6 +1105,7 @@ static void _qcrypto_cra_aead_aes_exit(struct crypto_aead *tfm)
 	ctx->ahash_aead_aes192_fb = NULL;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static int _disp_stats(int id)
 {
 	struct crypto_stat *pstat;
@@ -1276,6 +1279,7 @@ static int _disp_stats(int id)
 				i, cp->cpu_req[i]);
 	return len;
 }
+#endif
 
 static void _qcrypto_remove_engine(struct crypto_engine *pengine)
 {
@@ -5408,6 +5412,7 @@ static struct platform_driver _qualcomm_crypto = {
 	},
 };
 
+#ifdef CONFIG_DEBUG_FS
 static int _debug_qcrypto;
 
 static int _debug_stats_open(struct inode *inode, struct file *file)
@@ -5496,15 +5501,16 @@ err:
 	debugfs_remove_recursive(_debug_dent);
 	return rc;
 }
+#endif
 
 static int __init _qcrypto_init(void)
 {
-	int rc;
 	struct crypto_priv *pcp = &qcrypto_dev;
 
-	rc = _qcrypto_debug_init();
-	if (rc)
-		return rc;
+#ifdef CONFIG_DEBUG_FS
+	_qcrypto_debug_init();
+#endif
+
 	INIT_LIST_HEAD(&pcp->alg_list);
 	INIT_LIST_HEAD(&pcp->engine_list);
 	init_llist_head(&pcp->ordered_resp_list);
@@ -5529,7 +5535,9 @@ static int __init _qcrypto_init(void)
 static void __exit _qcrypto_exit(void)
 {
 	pr_debug("%s Unregister QCRYPTO\n", __func__);
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(_debug_dent);
+#endif
 	platform_driver_unregister(&_qualcomm_crypto);
 }
 
