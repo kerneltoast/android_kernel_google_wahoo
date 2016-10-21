d8beb95b965ff74149f18ed9814375fd90c24372
Sultan Alsawaf <sultan@kerneltoast.com>
2016-10-21 01:37:16 -0700

    proc: Remove SafetyNet flags from /proc/cmdline
    Userspace parses this and sets the ro.boot.* props accordingly, which in
    turn trips SafetyNet when the reported values are incorrect.
    
    Hide the cmdline flags checked by SafetyNet to prevent it from failing a
    device. These flags were found by extracting the latest snet.jar and searching
    for 'ro.boot.' strings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -21,8 +24,36 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void remove_flag(char *cmd, const char *flag)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are removed */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memmove(start_addr, end_addr + 1, strlen(end_addr));
+		else
+			*(max(cmd, start_addr - 1)) = '\0';
+	}
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+	remove_flag(cmd, "androidboot.verifiedbootstate=");
+	remove_flag(cmd, "androidboot.veritymode=");
+}
+
 static int __init proc_cmdline_init(void)
 {
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Remove various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
+	 */
+	remove_safetynet_flags(new_command_line);
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
