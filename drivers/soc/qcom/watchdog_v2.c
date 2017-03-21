ef33555fdf32cce5c14d28af11f8d3c61f5406ce
Sultan Alsawaf <sultan@kerneltoast.com>
2017-03-21 01:06:00 -0700

    soc: qcom: watchdog_v2: Fix memory leaks when memory_dump_v2 isn't built
    When the memory_dump_v2.c driver isn't built (CONFIG_MSM_MEMORY_DUMP_V2=n),
    an inline version of msm_dump_data_register() will be used that does
    nothing. This means that all memory allocated with the intention of going
    to msm_dump_data_register() will be leaked.
    
    Fix this by ignoring the entire code block when CONFIG_MSM_MEMORY_DUMP_V2
    is disabled.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -639,7 +639,7 @@ static void configure_bark_dump(struct msm_watchdog_data *wdog_dd)
 			 * without saving registers.
 			 */
 		}
-	} else {
+	} else if (IS_ENABLED(CONFIG_MSM_MEMORY_DUMP_V2)) {
 		cpu_data = kzalloc(sizeof(struct msm_dump_data) *
 				   num_present_cpus(), GFP_KERNEL);
 		if (!cpu_data) {
