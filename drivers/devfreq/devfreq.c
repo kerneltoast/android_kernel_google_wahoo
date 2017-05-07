8d5f559714ea261ddc7056c4a683de64a0537c17
Sultan Alsawaf <sultanxda@gmail.com>
2017-05-07 13:23:20 -0700

    PM / devfreq: Boost "soc:qcom,cpubw" on screen wake
    This makes our DDR bus use its max bandwidth when the screen is turned on.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -45,6 +45,7 @@ static DEFINE_MUTEX(devfreq_list_lock);
 
 /* List of devices to boost when the screen is woken */
 static const char *boost_devices[] = {
+	"soc:qcom,cpubw",
 };
 
 #define WAKE_BOOST_DURATION_MS (3000)
