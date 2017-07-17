2fb2fa269f9ac00faf9a91d371998180ea06e391
Sultan Alsawaf <sultan@kerneltoast.com>
2017-07-16 19:52:13 -0700

    msm: clock-osm: Use CLKFLAG_NO_RATE_CACHE for pwrcl and perfcl clk
    Clusters can have frequency change requests ignored when
    they come online without having this flag set.
    This can result in clusters running at their default frequency
    instead of the frequency chosen by the CPU governor.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/clk/msm/clock-osm.c
+++ drivers/clk/msm/clock-osm.c
@@ -773,6 +773,7 @@ static struct clk_osm pwrcl_clk = {
 	.cpu_reg_mask = 0x3,
 	.c = {
 		.dbg_name = "pwrcl_clk",
+		.flags = CLKFLAG_NO_RATE_CACHE,
 		.ops = &clk_ops_cpu_osm,
 		.parent = &xo_ao.c,
 		CLK_INIT(pwrcl_clk.c),
@@ -784,6 +785,7 @@ static struct clk_osm perfcl_clk = {
 	.cpu_reg_mask = 0x103,
 	.c = {
 		.dbg_name = "perfcl_clk",
+		.flags = CLKFLAG_NO_RATE_CACHE,
 		.ops = &clk_ops_cpu_osm,
 		.parent = &xo_ao.c,
 		CLK_INIT(perfcl_clk.c),
