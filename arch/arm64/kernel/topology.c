7d863bbcb120de4e7037404616923478b7967d71
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-13 14:01:57 -0700

    cpu: Silence log spam when a CPU is brought up
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -305,7 +305,7 @@ static void update_cpu_capacity(unsigned int cpu)
 
 	set_capacity_scale(cpu, capacity);
 
-	pr_info("CPU%d: update cpu_capacity %lu\n",
+	pr_debug("CPU%d: update cpu_capacity %lu\n",
 		cpu, arch_scale_cpu_capacity(NULL, cpu));
 }
 
