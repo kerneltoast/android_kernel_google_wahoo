06432e86626c63fb8db6e4a65146d4f113a3777c
Sultan Alsawaf <sultan@kerneltoast.com>
2018-05-29 17:24:10 -0700

    input: fpc1020: Mark IRQ as performance critical
    In order to improve fingerprint wake performance, mark the fingerprint
    reader's IRQ as performance-critical.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/misc/fpr_FingerprintCard/fpc1020_platform_tee.c
+++ drivers/misc/fpr_FingerprintCard/fpc1020_platform_tee.c
@@ -558,7 +558,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	atomic_set(&fpc1020->wakeup_enabled, 0);
 
-	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL;
 	if (of_property_read_bool(dev->of_node, "fpc,enable-wakeup")) {
 		irqf |= IRQF_NO_SUSPEND;
 		device_init_wakeup(dev, 1);
