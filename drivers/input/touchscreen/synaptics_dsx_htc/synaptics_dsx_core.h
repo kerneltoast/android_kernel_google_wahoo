8a25b8c63effa3f4929bb29f70130025e2c45557
Siarhei Vishniakou <svv@google.com>
2018-06-28 10:53:18 +0100

    input: touchscreen: wahoo: Require low latency
    Currently, CPU can enter deep sleep while waiting for data on the i2c
    bug. That means an i2c read of 8 bytes can take as long as 10 ms, which
    would overrun the desired interrupt handling time.
    Use pm qos to require low latency and prevent CPU from entering deep
    sleep while handling touch interrupts.
    
    Bug: 110939384
    Test: look at the i2c read traces:
    1) ./external/chromium-trace/systrace.py --atrace-categories=view,wm,am,app,gfx,i2c,sched,irq,video,power,input,workq,freq
    2) perform a fling
    3) review the i2c read duration on the touchscreen bus
    
    Change-Id: Icf8ab09324003a85af70517769ced3bae52f705c
    Signed-Off-By: Siarhei Vishniakou <svv@google.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/input/touchscreen/synaptics_dsx_htc/synaptics_dsx_core.h
+++ drivers/input/touchscreen/synaptics_dsx_htc/synaptics_dsx_core.h
@@ -39,6 +39,7 @@
 #define SYNAPTICS_DSX_DRIVER_PRODUCT (SYNAPTICS_DS4 | SYNAPTICS_DS5)
 #define SYNAPTICS_DSX_DRIVER_VERSION 0x2066
 
+#include <linux/pm_qos.h>
 #include <linux/version.h>
 #ifdef CONFIG_FB
 #include <linux/notifier.h>
@@ -516,6 +517,7 @@ struct synaptics_rmi4_data {
 			bool enable);
 	void (*report_touch)(struct synaptics_rmi4_data *rmi4_data,
 			struct synaptics_rmi4_fn *fhandler);
+	struct pm_qos_request pm_qos_req;
 };
 
 struct synaptics_dsx_bus_access {
