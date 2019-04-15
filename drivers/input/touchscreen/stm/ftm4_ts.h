19716ab711c38a4067bc2b4fccd404c0c75e46ec
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 18:17:42 -0700

    input: touchscreen: Offload fb notifier callbacks onto workers
    Framebuffer notifier callbacks block framebuffer state changes until
    the callbacks are finished executing. This means that any significant
    delays in a framebuffer notifier callback will stall the screen when
    powering it on.
    
    In order to eliminate screen-on delays from these framebuffer notifiers,
    offload their callbacks onto workers.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/input/touchscreen/stm/ftm4_ts.h
+++ drivers/input/touchscreen/stm/ftm4_ts.h
@@ -327,6 +327,8 @@ struct fts_ts_info {
 	bool enabled;
 #if defined(CONFIG_FB)
 	struct notifier_block fb_notif;
+	struct work_struct resume_work;
+	struct work_struct suspend_work;
 #endif
 #ifdef FEATURE_FTS_PRODUCTION_CODE
 	struct device *pdc_dev_ts;
