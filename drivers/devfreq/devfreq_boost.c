13b25cd29e6c56b09dafaf0d004911af0be48e59
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-17 21:39:16 -0700

    devfreq_boost: Mark boost kthreads as performance critical
    The boost kthreads are performance critical for obvious reasons.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -295,8 +295,8 @@ static int __init devfreq_boost_init(void)
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = d->devices + i;
 
-		thread[i] = kthread_run(devfreq_boost_thread, b,
-					"devfreq_boostd/%d", i);
+		thread[i] = kthread_run_perf_critical(devfreq_boost_thread, b,
+						      "devfreq_boostd/%d", i);
 		if (IS_ERR(thread[i])) {
 			ret = PTR_ERR(thread[i]);
 			pr_err("Failed to create kthread, err: %d\n", ret);
