7895ca9a959c6aebf7be318b35ce3d129448872b
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-29 10:56:11 -0700

    drv2624: Disable haptics while camera is in use
    The motion from the haptics are strong enough to distort camera images
    significantly. To mitigate this, disable haptics while the camera is in
    use.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -17,6 +17,7 @@
 #include "msm_camera_i2c_mux.h"
 #include <linux/regulator/rpm-smd-regulator.h>
 #include <linux/regulator/consumer.h>
+#include <linux/drv2624.h>
 
 /*fw update start*/
 #ifdef CONFIG_FW_UPDATE
@@ -217,6 +218,8 @@ int msm_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 		return -EINVAL;
 	}
 
+	drv2624_enable_haptics();
+
 	/* Power down secure session if it exist*/
 	if (s_ctrl->is_secure)
 		msm_camera_tz_i2c_power_down(sensor_i2c_client);
@@ -296,6 +299,9 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 			break;
 		}
 	}
+
+	if (!rc)
+		drv2624_disable_haptics();
 
 	print_time(__func__, sensor_name);
 
