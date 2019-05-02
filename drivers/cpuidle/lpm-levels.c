22a17f0bd6467e6b8dc024b48723161858322ec9
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-01 17:54:26 -0700

    cpuidle: lpm-levels: Allow exit latencies equal to target latencies
    This allows pm_qos votes with, say, 100 us for example to select power
    levels with exit latencies equal to 100 us. The extra microsecond of
    exit latency doesn't hurt.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -696,7 +696,7 @@ static int cpu_power_select(struct cpuidle_device *dev,
 
 		lvl_latency_us = pwr_params->latency_us;
 
-		if (latency_us < lvl_latency_us)
+		if (latency_us <= lvl_latency_us)
 			break;
 
 		if (next_event_us) {
@@ -1042,7 +1042,7 @@ static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 					&level->num_cpu_votes))
 			continue;
 
-		if (from_idle && latency_us < pwr_params->latency_us)
+		if (from_idle && latency_us <= pwr_params->latency_us)
 			break;
 
 		if (sleep_us < pwr_params->time_overhead_us)
