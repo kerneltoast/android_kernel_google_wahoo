fe23bc08876e99a6d649afb94b3342efa2fcdbb7
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-04 20:42:16 -0700

    staging: sync: Don't copy fence names by default
    Copying fence debug names for frequent users of sync fences causes a
    measurably significant portion of CPU time to be spent on this strlcpy.
    
    #ifdef out the sync fence name copy by default to improve performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -164,7 +164,9 @@ static struct sync_fence *sync_fence_alloc(int size, const char *name)
 		goto err;
 
 	kref_init(&fence->kref);
+#ifdef CONFIG_SYNC_DEBUG
 	strlcpy(fence->name, name, sizeof(fence->name));
+#endif
 
 	init_waitqueue_head(&fence->wq);
 
