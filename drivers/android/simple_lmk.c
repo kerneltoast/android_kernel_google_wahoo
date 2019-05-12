2b911f86a5fd99c60400c3c6af5ac27f07a876c8
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-12 13:12:21 -0700

    simple_lmk: Mark reclaim kthread as performance critical
    Simple LMK's reclaim thread needs to run as quickly as possible to
    reduce memory allocation latency when memory pressure is high. Mark it
    as performance critical to schedule it on faster CPUs.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -316,7 +316,8 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 	if (cmpxchg(&init_done, false, true))
 		return 0;
 
-	thread = kthread_run(simple_lmk_reclaim_thread, NULL, "simple_lmkd");
+	thread = kthread_run_perf_critical(simple_lmk_reclaim_thread, NULL,
+					   "simple_lmkd");
 	BUG_ON(IS_ERR(thread));
 
 	return 0;
