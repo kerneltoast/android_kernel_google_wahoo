501ec28af147113fb26adfbb5dcb6306a757c65b
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-27 19:20:42 -0700

    msm: camera: Undo pm_qos request upon error
    If anything after msm_pm_qos_update_request(CAMERA_DISABLE_PC_LATENCY)
    fails, the pm_qos request won't be undone and will waste power. Make
    sure it gets unset in the error path to fix this.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/platform/msm/camera_v2/camera/camera.c
+++ drivers/media/platform/msm/camera_v2/camera/camera.c
@@ -701,6 +701,7 @@ post_fail:
 command_ack_q_fail:
 	msm_destroy_session(pvdev->vdev->num);
 session_fail:
+	msm_pm_qos_update_request(CAMERA_ENABLE_PC_LATENCY);
 	pm_relax(&pvdev->vdev->dev);
 stream_fail:
 	camera_v4l2_vb2_q_release(filep);
