19013e4a55012caede496aec4f59e9b518c8237b
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-25 10:53:43 -0700

    iommu: arm-smmu: Free pgtbl ops when domain contexts are destroyed
    When a domain context is initialized, pgtbl ops are allocated, but they
    aren't freed when the domain context is destroyed, causing memory to be
    leaked. Fix the memory leak by freeing pgtbl ops when domain contexts
    are destroyed.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -1894,6 +1894,25 @@ out:
 	return ret;
 }
 
+static void arm_smmu_free_pgtbl_ops(struct arm_smmu_domain *smmu_domain)
+{
+	/*
+	 * Free the domain resources. We assume that all devices have
+	 * already been detached.
+	 */
+	if (smmu_domain->pgtbl_ops) {
+		free_io_pgtable_ops(smmu_domain->pgtbl_ops);
+		/* unassign any freed page table memory */
+		if (arm_smmu_is_master_side_secure(smmu_domain)) {
+			arm_smmu_secure_domain_lock(smmu_domain);
+			arm_smmu_secure_pool_destroy(smmu_domain);
+			arm_smmu_unassign_table(smmu_domain);
+			arm_smmu_secure_domain_unlock(smmu_domain);
+		}
+		smmu_domain->pgtbl_ops = NULL;
+	}
+}
+
 static void arm_smmu_destroy_domain_context(struct iommu_domain *domain)
 {
 	struct arm_smmu_domain *smmu_domain = to_smmu_domain(domain);
@@ -1921,6 +1940,7 @@ free_irqs:
 		free_irq(irq, domain);
 	}
 
+	arm_smmu_free_pgtbl_ops(smmu_domain);
 	arm_smmu_free_context_idx(smmu, cfg->cbndx);
 	smmu_domain->smmu = NULL;
 	cfg->cbndx = INVALID_CBNDX;
@@ -1965,22 +1985,7 @@ static void arm_smmu_domain_free(struct iommu_domain *domain)
 {
 	struct arm_smmu_domain *smmu_domain = to_smmu_domain(domain);
 
-	/*
-	 * Free the domain resources. We assume that all devices have
-	 * already been detached.
-	 */
-	if (smmu_domain->pgtbl_ops) {
-		free_io_pgtable_ops(smmu_domain->pgtbl_ops);
-		/* unassign any freed page table memory */
-		if (arm_smmu_is_master_side_secure(smmu_domain)) {
-			arm_smmu_secure_domain_lock(smmu_domain);
-			arm_smmu_secure_pool_destroy(smmu_domain);
-			arm_smmu_unassign_table(smmu_domain);
-			arm_smmu_secure_domain_unlock(smmu_domain);
-		}
-		smmu_domain->pgtbl_ops = NULL;
-	}
-
+	arm_smmu_free_pgtbl_ops(smmu_domain);
 	kfree(smmu_domain);
 }
 
