25eb85bea9bf2d4c6242eeb09b386ca0f9b3bef6
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-29 21:23:36 -0700

    dmaengine: Fix memory leak in dma_async_device_register
    The atomic reference counter, idr_ref, is leaked when there are no
    channels available for a DMA device. Free idr_ref in this case to fix
    the memory leak.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/dma/dmaengine.c
+++ drivers/dma/dmaengine.c
@@ -945,6 +945,9 @@ int dma_async_device_register(struct dma_device *device)
 	dma_channel_rebalance();
 	mutex_unlock(&dma_list_mutex);
 
+	if (!chancnt)
+		kfree(idr_ref);
+
 	return 0;
 
 err_out:
