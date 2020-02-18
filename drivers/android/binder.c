800b785853a3f2f9fdb843569b79375aa0c484c7
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-17 22:43:20 -0800

    binder: Fix 4.19 binder compilation on 4.4
    It doesn't look like any real backporting work is needed: this works in
    its current state. It appears that the mmget_still_valid() check isn't
    needed in binder_alloc.c, since only a read lock on mmap_sem is held,
    rather than a write lock.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -63,8 +63,7 @@
 #include <linux/poll.h>
 #include <linux/debugfs.h>
 #include <linux/rbtree.h>
-#include <linux/sched/signal.h>
-#include <linux/sched/mm.h>
+#include <linux/sched.h>
 #include <linux/seq_file.h>
 #include <linux/uaccess.h>
 #include <linux/pid_namespace.h>
@@ -73,7 +72,7 @@
 #include <linux/ratelimit.h>
 
 #include <uapi/linux/android/binder.h>
-#include <uapi/linux/sched/types.h>
+#include <uapi/linux/eventpoll.h>
 
 #include <asm/cacheflush.h>
 
@@ -150,7 +149,7 @@ static DECLARE_WAIT_QUEUE_HEAD(binder_user_error_wait);
 static int binder_stop_on_user_error;
 
 static int binder_set_stop_on_user_error(const char *val,
-					 const struct kernel_param *kp)
+					 struct kernel_param *kp)
 {
 	int ret;
 
@@ -4814,7 +4813,7 @@ static int binder_thread_release(struct binder_proc *proc,
 	return active_transactions;
 }
 
-static __poll_t binder_poll(struct file *filp,
+static unsigned int binder_poll(struct file *filp,
 				struct poll_table_struct *wait)
 {
 	struct binder_proc *proc = filp->private_data;
@@ -5172,7 +5171,7 @@ static void binder_vma_close(struct vm_area_struct *vma)
 	binder_defer_work(proc, BINDER_DEFERRED_PUT_FILES);
 }
 
-static vm_fault_t binder_vm_fault(struct vm_fault *vmf)
+static int binder_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 {
 	return VM_FAULT_SIGBUS;
 }
