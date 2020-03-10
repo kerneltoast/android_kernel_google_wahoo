9cf26d7167846f6c908367a6a121f767dab1a6be
Sultan Alsawaf <sultan@kerneltoast.com>
2020-03-10 12:15:38 -0700

    qcacld-3.0: Fix potentially uninitialized session ID usage
    When csr_roam_get_session_id_from_bssid() fails, it leaves sessionId
    uninitialized. Since CSR_GET_SESSION() is done without checking the
    validity of sessionId, this can result in dereferencing a garbage value.
    Fix it by exiting early when csr_roam_get_session_id_from_bssid()
    returns an error.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/staging/qcacld-3.0/core/sme/src/csr/csr_api_roam.c
+++ drivers/staging/qcacld-3.0/core/sme/src/csr/csr_api_roam.c
@@ -10495,6 +10495,8 @@ void csr_roam_joined_state_msg_processor(tpAniSirGlobal pMac, void *pMsgBuf)
 							(struct qdf_mac_addr *)
 							   pUpperLayerAssocCnf->
 							   bssId, &sessionId);
+		if (status == QDF_STATUS_E_FAILURE)
+			return;
 		pSession = CSR_GET_SESSION(pMac, sessionId);
 
 		if (!pSession) {
