0677dd4d298ca5db34edbef618acbfb3a36bc538
Sultan Alsawaf <sultan@kerneltoast.com>
2020-03-18 21:54:15 -0700

    irqchip/gic-v3-its: Put its_init() in __init section
    This fixes a section mismatch since its_init() is only used from inside
    the __init section.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/irqchip/irq-gic-v3-its.c
+++ drivers/irqchip/irq-gic-v3-its.c
@@ -1635,8 +1635,8 @@ static struct of_device_id its_device_id[] = {
 	{},
 };
 
-int its_init(struct device_node *node, struct rdists *rdists,
-	     struct irq_domain *parent_domain)
+int __init its_init(struct device_node *node, struct rdists *rdists,
+		    struct irq_domain *parent_domain)
 {
 	struct device_node *np;
 
