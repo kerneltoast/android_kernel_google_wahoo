bde6e2fec51509eec98719b7e32465d1782e4e73
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-07 23:46:46 -0700

    input: misc: vl53L0: Check VL53L010_get_vcsel_pulse_period() status
    VL53L010_get_vcsel_pulse_period() is used without checking its return
    value. It looks like it queries hardware, so it is definitely worth
    checking.
    
    This fixes the following warning:
    drivers/input/misc/vl53L0/src/vl53l010_api.c: In function 'VL53L010_GetMeasurementTimingBudgetMicroSeconds':
    drivers/input/misc/vl53L0/src/vl53l010_api.c:2746:47: error: 'CurrentVCSELPulsePeriodPClk' may be used uninitialized in this function [-Werror=maybe-uninitialized]
     2746 |  vcsel_period_reg = (vcsel_period_pclks >> 1) - 1;
          |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~^~~
    drivers/input/misc/vl53L0/src/vl53l010_api.c:1087:10: note: 'CurrentVCSELPulsePeriodPClk' was declared here
     1087 |  uint8_t CurrentVCSELPulsePeriodPClk;
          |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/input/misc/vl53L0/src/vl53l010_api.c
+++ drivers/input/misc/vl53L0/src/vl53l010_api.c
@@ -1096,19 +1096,21 @@ VL53L0_Error VL53L010_GetMeasurementTimingBudgetMicroSeconds(VL53L0_DEV Dev,
 	Status = VL53L010_GetWrapAroundCheckEnable(Dev, &Byte);
 
 	if (Status == VL53L0_ERROR_NONE) {
-		VL53L010_get_vcsel_pulse_period(Dev,
+		Status = VL53L010_get_vcsel_pulse_period(Dev,
 		&CurrentVCSELPulsePeriodPClk, 0);
-		CurrentVCSELPulsePeriod = VL53L010_encode_vcsel_period(
-			CurrentVCSELPulsePeriodPClk);
-
-		/* Read from register */
-		Status = VL53L0_RdWord(Dev, VL53L010_REG_RNGA_TIMEOUT_MSB,
-			&encodedTimeOut);
 		if (Status == VL53L0_ERROR_NONE) {
-			RangATimingBudgetMicroSeconds =
-			VL53L010_calc_ranging_wait_us(Dev,
-				encodedTimeOut,
-				CurrentVCSELPulsePeriod);
+			CurrentVCSELPulsePeriod = VL53L010_encode_vcsel_period(
+				CurrentVCSELPulsePeriodPClk);
+
+			/* Read from register */
+			Status = VL53L0_RdWord(Dev, VL53L010_REG_RNGA_TIMEOUT_MSB,
+				&encodedTimeOut);
+			if (Status == VL53L0_ERROR_NONE) {
+				RangATimingBudgetMicroSeconds =
+				VL53L010_calc_ranging_wait_us(Dev,
+					encodedTimeOut,
+					CurrentVCSELPulsePeriod);
+			}
 		}
 	}
 
@@ -1120,29 +1122,31 @@ VL53L0_Error VL53L010_GetMeasurementTimingBudgetMicroSeconds(VL53L0_DEV Dev,
 			MeasurementTimingBudgetMicroSeconds,
 				*pMeasurementTimingBudgetMicroSeconds);
 		} else {
-			VL53L010_get_vcsel_pulse_period(Dev,
+			Status = VL53L010_get_vcsel_pulse_period(Dev,
 			&CurrentVCSELPulsePeriodPClk, 1);
-			CurrentVCSELPulsePeriod = VL53L010_encode_vcsel_period(
-				CurrentVCSELPulsePeriodPClk);
-
-			/* Read from register */
-			Status = VL53L0_RdWord(Dev,
-			VL53L010_REG_RNGB1_TIMEOUT_MSB,
-						&encodedTimeOut);
 			if (Status == VL53L0_ERROR_NONE) {
-				RangBTimingBudgetMicroSeconds =
-				VL53L010_calc_ranging_wait_us(
-					Dev, encodedTimeOut,
-					CurrentVCSELPulsePeriod);
-			}
+				CurrentVCSELPulsePeriod = VL53L010_encode_vcsel_period(
+					CurrentVCSELPulsePeriodPClk);
 
-			*pMeasurementTimingBudgetMicroSeconds =
-				RangATimingBudgetMicroSeconds +
-				RangBTimingBudgetMicroSeconds +
-				7000;
-			VL53L010_SETPARAMETERFIELD(Dev,
-			MeasurementTimingBudgetMicroSeconds,
-				*pMeasurementTimingBudgetMicroSeconds);
+				/* Read from register */
+				Status = VL53L0_RdWord(Dev,
+				VL53L010_REG_RNGB1_TIMEOUT_MSB,
+							&encodedTimeOut);
+				if (Status == VL53L0_ERROR_NONE) {
+					RangBTimingBudgetMicroSeconds =
+					VL53L010_calc_ranging_wait_us(
+						Dev, encodedTimeOut,
+						CurrentVCSELPulsePeriod);
+				}
+
+				*pMeasurementTimingBudgetMicroSeconds =
+					RangATimingBudgetMicroSeconds +
+					RangBTimingBudgetMicroSeconds +
+					7000;
+				VL53L010_SETPARAMETERFIELD(Dev,
+				MeasurementTimingBudgetMicroSeconds,
+					*pMeasurementTimingBudgetMicroSeconds);
+			}
 		}
 	}
 
