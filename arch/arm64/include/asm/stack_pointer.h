8f4f6fad56fdf39e7f557e864363245ad05caa08
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-07 23:46:47 -0700

    arm64: Use a macro to silence current_stack_pointer warning with LTO
    GCC spams the following warning during link time with LTO:
    arch/arm64/include/asm/stack_pointer.h:7: warning: register of 'current_stack_pointer' used for multiple global register variables
    
    Resolve it by tricking GCC with a macro so that it thinks the register
    value is only used once (i.e., inside the macro).
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/include/asm/stack_pointer.h
+++ arch/arm64/include/asm/stack_pointer.h
@@ -4,6 +4,11 @@
 /*
  * how to get the current stack pointer from C
  */
-register unsigned long current_stack_pointer asm ("sp");
+#define current_stack_pointer \
+({									\
+	register unsigned long __current_stack_pointer asm ("sp");	\
+									\
+	__current_stack_pointer;					\
+})
 
 #endif /* __ASM_STACK_POINTER_H */
