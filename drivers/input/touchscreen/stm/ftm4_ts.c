5fff74699e9db54457640e2abc39574aeac19e5f
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-22 19:09:47 -0800

    input: touchscreen: wahoo: Mark IRQs as performance critical
    Touch latency is pretty important; let's service the touch IRQs and
    associated kthreads on fast CPUs to improve touch responsiveness.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/input/touchscreen/stm/ftm4_ts.c
+++ drivers/input/touchscreen/stm/ftm4_ts.c
@@ -1949,6 +1949,7 @@ static int fts_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 		goto err_enable_irq;
 	}
 
+	info->board->irq_type |= IRQF_PERF_CRITICAL;
 	retval = request_threaded_irq(info->irq, fts_hard_interrupt_handler,
 			fts_interrupt_handler, info->board->irq_type,
 			FTS_TS_DRV_NAME, info);
