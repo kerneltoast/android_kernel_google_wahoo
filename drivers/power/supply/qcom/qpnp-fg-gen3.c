2f3850701c74e064ab04e851e555a5fb8b797ab9
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-22 19:09:02 -0800

    qpnp-fg-gen3: Limit how frequently fg data can be queried
    Querying the fuelgauge for data is expensive, so doing it frequently can
    result in a significant increase in power consumption. The fuelgauge
    also only generates fresh statistics once per second.
    
    Limit how frequently data queries can be sent to 2 seconds in order to
    prevent fuelgauge queries from consuming too much power. Only do this
    while there is no USB cable connected to the device, since the charging
    drivers need fresh statistics when there's a USB cable connected.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -408,6 +408,8 @@ static bool fg_sram_dump;
 
 static int fg_get_battery_current(struct fg_chip *chip, int *val);
 
+#define FG_RATE_LIM_MS (2 * MSEC_PER_SEC)
+
 /* All getters HERE */
 
 #define VOLTAGE_15BIT_MASK	GENMASK(14, 0)
@@ -1321,6 +1323,15 @@ static bool batt_psy_initialized(struct fg_chip *chip)
 	fg_notify_charger(chip);
 
 	return true;
+}
+
+static bool usb_psy_initialized(struct fg_chip *chip)
+{
+	if (chip->usb_psy)
+		return true;
+
+	chip->usb_psy = power_supply_get_by_name("usb");
+	return chip->usb_psy;
 }
 
 static bool is_parallel_charger_available(struct fg_chip *chip)
@@ -3343,7 +3354,35 @@ static int fg_psy_get_property(struct power_supply *psy,
 				       union power_supply_propval *pval)
 {
 	struct fg_chip *chip = power_supply_get_drvdata(psy);
+	struct fg_saved_data *sd = chip->saved_data + psp;
+	union power_supply_propval typec_sts = { .intval = -1 };
 	int rc = 0;
+
+	switch (psp) {
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+	case POWER_SUPPLY_PROP_RESISTANCE_ID:
+	case POWER_SUPPLY_PROP_VOLTAGE_MAX_DESIGN:
+	case POWER_SUPPLY_PROP_CYCLE_COUNT_ID:
+	case POWER_SUPPLY_PROP_CHARGE_NOW:
+	case POWER_SUPPLY_PROP_CHARGE_FULL:
+	case POWER_SUPPLY_PROP_SOC_REPORTING_READY:
+		/* These props don't require a fg query; don't ratelimit them */
+		break;
+	default:
+		if (!sd->last_req_expires)
+			break;
+
+		if (usb_psy_initialized(chip))
+			power_supply_get_property(chip->usb_psy,
+				POWER_SUPPLY_PROP_TYPEC_MODE, &typec_sts);
+
+		if (typec_sts.intval == POWER_SUPPLY_TYPEC_NONE &&
+			time_before(jiffies, sd->last_req_expires)) {
+			*pval = sd->val;
+			return 0;
+		}
+		break;
+	}
 
 	switch (psp) {
 	case POWER_SUPPLY_PROP_CAPACITY:
@@ -3428,6 +3467,9 @@ static int fg_psy_get_property(struct power_supply *psy,
 
 	if (rc < 0)
 		return -ENODATA;
+
+	sd->val = *pval;
+	sd->last_req_expires = jiffies + msecs_to_jiffies(FG_RATE_LIM_MS);
 
 	return 0;
 }
